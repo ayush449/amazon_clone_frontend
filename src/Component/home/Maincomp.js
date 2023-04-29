a3edf5bfc4c9b1cecd86bd8426fa64a1bef9d324
Ayush <ayushtiwari1180@gmail.com>
2023-04-30 02:17:22 +0530

    redux used to manage state

--- src/Component/home/Maincomp.js
+++ src/Component/home/Maincomp.js
@@ -1,17 +1,27 @@
-import React from 'react'
+import React , { useEffect } from 'react'
 import Banner from './Banner'
 import "./home.css";
 import Slide from './Slide';
+import {getProducts} from "../redux/action/action"
+import { useSelector, useDispatch } from "react-redux";
 
 function MainComp() {
-  return (
+
+    const { products } = useSelector(state => state.getproductsdata);
+    console.log(products);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(getProducts());
+    }, [dispatch])
+    return (
     <div className="home_section">
         <div className="banner_part">
                     <Banner />
                 </div>
                 <div className="slide_part">
                     <div className="left_slide">
-                        <Slide title="Deal Of The Day"  />
+                        <Slide title="Deal Of The Day" products ={products} />
                     </div>
                     <div className="right_slide">
                         <h4>Festive latest launches</h4>
@@ -19,11 +29,11 @@ function MainComp() {
                         <a href="example.com">see more</a>
                     </div>
                 </div>
-                <Slide title="Today's Deal"/>
+                <Slide title="Today's Deal" products ={products}/>
                 <div className="center_img">
                     <img src="https://m.media-amazon.com/images/G/31/AMS/IN/970X250-_desktop_banner.jpg" alt="" />
                 </div>
-                <Slide title="Best Seller"/>
+                <Slide title="Best Seller" products ={products} />
                 <Slide title="Upto 80% off"/>
       
     </div>
